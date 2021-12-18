412f8a55d51b04867c022e1690fad1e41136fb7d
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2021-12-18 23:49:55 +0500

    some

--- assets/js/codeEditor.js
+++ assets/js/codeEditor.js
@@ -1,44 +1,76 @@
-var htmleditor = CodeMirror.fromTextArea(document.querySelector('#html'), {
+var htmlEditor = CodeMirror.fromTextArea(document.querySelector('#html'), {
     lineNumbers: true,
     tabSize: 4,
     mode: 'htmlmixed',
     lineWrapping: true,
     smartIndent: true,
     addModeClass: true,
-    matchBrackets: true
+    matchBrackets: true,
+    theme: 'blackboard'
 });
-var csseditor = CodeMirror.fromTextArea(document.querySelector('#css'), {
+var cssEditor = CodeMirror.fromTextArea(document.querySelector('#css'), {
     lineNumbers: true,
     tabSize: 4,
     mode: 'css',
     lineWrapping: true,
     smartIndent: true,
     addModeClass: true,
-    matchBrackets: true
+    matchBrackets: true,
+    theme: 'blackboard'
 });
-var jseditor = CodeMirror.fromTextArea(document.querySelector('#js'), {
+var jsEditor = CodeMirror.fromTextArea(document.querySelector('#js'), {
     lineNumbers: true,
     tabSize: 4,
     mode: 'javascript',
     lineWrapping: true,
     smartIndent: true,
     addModeClass: true,
-    matchBrackets: true
+    matchBrackets: true,
+    theme: 'blackboard'
 });
 const iframe = document.querySelector('iframe')
-emmetCodeMirror(csseditor);
-emmetCodeMirror(htmleditor);
+emmetCodeMirror(cssEditor);
+emmetCodeMirror(htmlEditor);
 
 function res() {
     let t = iframe.contentWindow.document;
-    html = htmleditor.getValue();
-    css = `<style>${csseditor.getValue()}</style>`
-    js = '<scri' + 'pt>' + jseditor.getValue() + '</scri' + 'pt>'
-    t.write('')
-    t.write(html + css + js)
-    console.log(html, css, js)
+    html = htmlEditor.getValue();
+    css = cssEditor.getValue() ? `<style>${cssEditor.getValue()}</style>` : '';
+    js = jsEditor.getValue() ? '<scri' + 'pt>' + jsEditor.getValue() + '</scri' + 'pt>' : '';
+    if (t.body) t.body.innerHTML = '';
+    if (t.head) t.head.innerHTML = '';
+    t.write(html + css + js);
+    console.log('Generated successfully');
 }
-const htmlCode = document.querySelector('#html-code')
-htmlCode.onclick = function () {
-    htmleditor.focus()
-}
+const htmlCodeContainer = document.querySelector('#htmlCode');
+htmlCodeContainer.onclick = function () {
+    htmlEditor.focus();
+};
+const cssCodeContainer = document.querySelector('#cssCode');
+cssCodeContainer.onclick = function () {
+    cssEditor.focus();
+};
+const jsCodeContainer = document.querySelector('#jsCode');
+jsCodeContainer.onclick = function () {
+    jsEditor.focus();
+};
+
+window.addEventListener('keydown', function (e) {
+    if (e.ctrlKey && e.keyCode == 13) {
+        res()
+    }
+})
+// const resizer = document.getElementById('resizer');
+// const editorCone = document.querySelector('.editor-content')
+// resizer.onmousedown = function (e) {
+//     window.onmousemove = function (e) {
+//         editorCone.style.width = e.clientX + 'px'
+//     }
+// }
+// resizer.onmouseup = function (e) {
+//     window.onmousemove = null
+// }
+// window.onmouseup = function () {
+//     window.onmousemove = null
+
+// }
